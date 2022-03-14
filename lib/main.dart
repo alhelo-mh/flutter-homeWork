75d0524e4cfc7df7650bfbc987fb6bffb5a4af74
mohammed alhelou <60815317+alhelo-mh@users.noreply.github.com>
2022-03-14 18:15:28 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,42 +18,47 @@ class IugScreen extends StatelessWidget {
           backgroundColor: Color.fromARGB(166, 129, 4, 167),
           title: Text('عاصمة فلسطين',
               style: TextStyle(color: Colors.white, fontSize: 30))),
-      body: Column(
-        children: [
-          Image.asset('../assets/images/quds.jpg'),
-          SizedBox(
-            height: 20,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset('../assets/images/quds.jpg'),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                child: Text('مدينة القدس',
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 97, 94, 94), fontSize: 30)),
+                alignment: Alignment.center,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              page_main(
+                label: 'القدس  ',
+                value: 'الاسم',
+              ),
+              page_main(
+                label: '١٢٥ كم ',
+                value: 'المساحة',
+              ),
+              page_main(
+                label: '٣٥٨٠٠٠ نسمة	 ',
+                value: 'السكان',
+              ),
+              page_main(
+                label: 'فلسطين ',
+                value: 'الدولة',
+              ),
+              page_main(
+                label: 'محمد ماهر الحلو ',
+                value: 'اسم الطالب',
+              ),
+            ],
           ),
-          Container(
-            child: Text('مدينة القدس',
-                style: TextStyle(
-                    color: Color.fromARGB(255, 97, 94, 94), fontSize: 30)),
-            alignment: Alignment.center,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          page_main(
-            label: 'القدس  ',
-            value: 'الاسم',
-          ),
-          page_main(
-            label: '١٢٥ كم ',
-            value: 'المساحة',
-          ),
-          page_main(
-            label: '٣٥٨٠٠٠ نسمة	 ',
-            value: 'السكان',
-          ),
-          page_main(
-            label: 'فلسطين ',
-            value: 'الدولة',
-          ),
-          page_main(
-            label: 'عمر أحمد علي ',
-            value: 'اسم الطالب',
-          ),
-        ],
+        ),
       ),
     );
   }
